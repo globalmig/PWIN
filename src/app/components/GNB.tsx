09045f43569a88525910586b532c21d12930e07d
연구개발전담부 <wdpb@mig.kr>
2025-05-29 16:54:59 +0900

    design: Add ActiverMenu design

--- src/app/components/GNB.tsx
+++ src/app/components/GNB.tsx
@@ -41,28 +41,34 @@ const mobileMenuItems = [
 ];
 
 export default function GNB() {
+  // 모바일 메뉴 상태 관리
   const [isOpen, setOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("");
 
   const toggleMenu = () => {
     setOpen(!isOpen);
   };
 
   return (
-    <div className="flex mx-auto bg-white opacity-90 mt-10 justify-between items-center w-full  z-[100] relative">
+    <div className="flex mx-auto bg-white opacity-90 mt-10 justify-between items-center w-full z-[100] relative">
       {/* 데스크톱 메뉴 */}
       <div className="hidden md:flex w-full justify-center">
         <header className="bg-white opacity-90 w-full  mx-auto relative z-[101]">
           <div className="relative group">
             {/* 상위 메뉴 */}
-            <ul className="flex justify-center items-center mx-auto text-xl font-medium py-6 px-16 gap-20">
+            <ul className="flex justify-center items-center mx-auto text-xl font-medium pb-4 px-16 gap-20">
               <li>
-                <Link href={"/"}>
+                <Link href={"/"} onClick={() => setActiveMenu(activeMenu === "home" ? "" : "home")}>
                   <Image src={"/images/logo.png"} alt="Logo" width={100} height={100} className="mr-10 py-6" />
                 </Link>
               </li>
               <ul className="flex w-full justify-between max-w-[960px] ">
                 {desktopMenuItems.map((item) => (
-                  <li>
+                  <li
+                    key={item.name}
+                    onClick={() => setActiveMenu(item.name)}
+                    className={`cursor-pointer hover:font-bold transition-colors duration-200 pb-1 ${activeMenu === item.name ? "text-lime-700 font-bold border-b-2 border-lime-600" : "text-black"}`}
+                  >
                     <Link href={item.href}>{item.name}</Link>
                   </li>
                 ))}
@@ -70,16 +76,16 @@ export default function GNB() {
             </ul>
 
             {/* 하위 메뉴 - 호버 시만 표시 */}
-            <div className="absolute hidden w-full mx-auto bg-white placeholder-opacity-95 text-xl text-black py-16 px-16 shadow-xl border-t-2 group-hover:flex justify-center z-[9999]">
+            <div className="absolute hidden w-full mx-auto bg-white placeholder-opacity-95 text-xl text-black pt-12 pb-16 px-16 shadow-xl border-t-2 group-hover:flex justify-center z-[9999]">
               <ul className="w-full flex justify-center gap-12">
                 {/* 빈공간 채우기용 DON'T REMOVE THIS CODE. */}
-                <li className="w-[140px] md:w-[120px] mr-12 py-6"></li>
+                <li className="w-[140px] md:w-[120px] mr-12 py-4"></li>
                 <ul className="flex w-full justify-between max-w-[960px]">
                   {/* 회사 소개 하위 카테고리 */}
                   <ul className="flex flex-col gap-9 items-center ">
                     {CompanySubMenuItems.map((item) => (
-                      <Link href={item.href}>
-                        <li>{item.name}</li>
+                      <Link href={item.href} key={item.name}>
+                        <li className="hover:scale-150 hover:font-semibold transition-transform duration-200 cursor-pointer">{item.name}</li>
                       </Link>
                     ))}
                   </ul>
@@ -87,8 +93,8 @@ export default function GNB() {
                   {/* 제품 하위 카테고리 */}
                   <ul className="flex flex-col gap-9 items-center">
                     {ProductsSubMenuItems.map((item) => (
-                      <Link href={item.href}>
-                        <li>{item.name}</li>
+                      <Link href={item.href} key={item.name}>
+                        <li className="hover:scale-150 hover:font-semibold transition-transform duration-200 cursor-pointer">{item.name}</li>
                       </Link>
                     ))}
                   </ul>
@@ -96,8 +102,8 @@ export default function GNB() {
                   {/* 공법소개 하위 카테고리 */}
                   <ul className="flex flex-col gap-9 items-center">
                     {ConstructionSubMenuItems.map((item) => (
-                      <Link href={item.href}>
-                        <li>{item.name}</li>
+                      <Link href={item.href} key={item.name}>
+                        <li className="hover:scale-150 hover:font-semibold transition-transform duration-200 cursor-pointer">{item.name}</li>
                       </Link>
                     ))}
                   </ul>
@@ -105,8 +111,8 @@ export default function GNB() {
                   {/* 갤러리 하위 카테고리 */}
                   <ul className="flex flex-col gap-9 items-center">
                     {GallerySubMenuItems.map((item) => (
-                      <Link href={item.href}>
-                        <li>{item.name}</li>
+                      <Link href={item.href} key={item.name}>
+                        <li className="hover:scale-150 hover:font-semibold transition-transform duration-200 cursor-pointer">{item.name}</li>
                       </Link>
                     ))}
                   </ul>
@@ -130,7 +136,7 @@ export default function GNB() {
         <nav className={`${isOpen ? "block" : "hidden"} absolute top-full right-0 w-screen bg-white shadow-lg border-t z-[9999]`}>
           <ul className="flex flex-col text-center text-xl font-medium p-8 gap-8">
             {mobileMenuItems.map((item) => (
-              <Link href={item.href} onClick={toggleMenu}>
+              <Link href={item.href} onClick={toggleMenu} key={item.name}>
                 <li className="border-b pb-4">{item.name}</li>
               </Link>
             ))}
